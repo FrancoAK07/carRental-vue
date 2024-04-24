5838d3fbd465f2f2b3e3cd7d38816e6122cf297b
Franco <franco_rivo7@hotmail.com>
2024-04-24 13:15:01 -0400

    Update [[userId]].js

--- functions/api/[[userId]].js
+++ functions/api/[[userId]].js
@@ -4,13 +4,13 @@ export function onRequestGet(context) {
 	const id = context.params.id;
 
 	if (!id) {
-		return new Response("Not found", { status: 404 });
+		return new Response("No id", { status: 404 });
 	}
 
-	const user = users.find((user) => user.id === id);
+	const user = users.find((user) => user.id === Number(id));
 
 	if (!user) {
-		return new Response("not foun", { status: 404 });
+		return new Response("Not found", { status: 404 });
 	}
 
 	return Response.json(user);
