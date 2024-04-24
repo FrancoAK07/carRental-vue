33cbecd4a871cc9b7c2be08f3fd3480beb8262a7
Franco <franco_rivo7@hotmail.com>
2024-04-24 18:37:12 -0400

    updated unRegisteredUsers

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -98,6 +98,6 @@
 	const car = await carsData.json();
 	console.log(car);
 
-	const person = await axios.get(`/api/unRegisteredUsersdata/1`);
+	const person = await axios.get(`/api/unRegisteredUsersData/cris@email`);
 	console.log(person);
 </script>
