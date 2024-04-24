58faf07c7ea5dab91a7bb8578bb1f03cf520dee3
Franco <franco_rivo7@hotmail.com>
2024-04-24 17:01:50 -0400

    Update [[id]].js

--- functions/api/data/[[id]].js
+++ functions/api/data/[[id]].js
@@ -1,17 +1,35 @@
-import users from "./users";
+// import users from "./users";
+
+// export function onRequestGet(context) {
+// 	const id = context.params.id;
+
+// 	if (!id) {
+// 		return new Response("No id", { status: 404 });
+// 	}
+
+// 	const user = users.find((user) => user.id === id);
+
+// 	if (!user) {
+// 		return new Response("user not found", { status: 404 });
+// 	}
+
+// 	return Response.json(user);
+// }
+
+import posts from "./users";
 
 export function onRequestGet(context) {
 	const id = context.params.id;
 
 	if (!id) {
-		return new Response("No id", { status: 404 });
+		return new Response("Not found", { status: 404 });
 	}
 
-	const user = users.find((user) => user.id === id);
+	const post = posts.find((post) => post.id === Number(id));
 
-	if (!user) {
-		return new Response("user not found", { status: 404 });
+	if (!post) {
+		return new Response("Not found", { status: 404 });
 	}
 
-	return Response.json(user);
+	return Response.json(post);
 }
